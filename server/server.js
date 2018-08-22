cae11bcf16f9485b47d23e2fbc7347611cbecb78
Filip Bakos <filipbakos@gmail.com>
2018-08-22 14:52:35 +0200

    socket io

--- server/server.js
+++ server/server.js
@@ -1,15 +1,31 @@
-const path = require('path');
+const path = require('path'); //netreba instalovat
+const http = require('http'); //netreba instalovat
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public' );
 const port = process.env.PORT || 3000;
 
-const app = express();
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+	console.log('new user connected');
+
+	socket.on('disconnect',() => {
+		console.log('User disconnected');
+	})
+});
+
+
+
+server.listen(port, () => {
 	console.log(`App je na porte ${port}`);
 });
+
+
 
 
